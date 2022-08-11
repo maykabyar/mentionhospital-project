584af3ac798bcfcd60d88e3cc7c67d152df90b6a
May Kabyar <kabitarmg21@gmail.com>
2022-08-11 11:23:39 +0630

    11/8/2022

--- js/app.js
+++ js/app.js
@@ -25,4 +25,111 @@ $(document).ready(function(){
 
 	// End Info Section
 
+	// Start ADV Section
+
+	$('#videos').click(function(){
+
+		var getmodal = $(this).data('bs-target');   // videomodal ya ml thu ko 1khr htel call ll ya dl
+		var getvideosrc = $(this).data('video');
+		var videourlwithauto = getvideosrc + '?autoplay=1';
+
+	   // #videomodal iframe ae lo 1khr htel call ll ya (iframe yae shae mr space par ya ml)
+		$(getmodal + ' iframe').attr('src',videourlwithauto);
+
+		$(getmodal + ' button.btn-close').click(function(){
+			$(getmodal + ' iframe').attr("src",getvideosrc);
+		});	
+
+		$(getmodal).click('hidden.bs.modal',function(){
+			$(getmodal + ' iframe').attr("src",getvideosrc);
+		})
+
+	});
+
+	// End ADV Section
+
+	// Start Premises Section
+
+	$('#lightslider').lightSlider({
+		item: 4,
+        loop: true,
+        slideMove: 1,
+        speed: 600,
+        // auto:true
+	}).play();
+
+	// End Premises Section
+
+	// Start Pricing Section
+
+	$(window).scroll(function(){
+
+		let getscroll = $(this).scrollTop();
+		// console.log(getscroll);
+
+		if(getscroll >= 2450){
+			$('.cardones').addClass('movelefts');
+			$('.cardtwos').addClass('movebottoms');
+			$('.cardthrees').addClass('moverights');
+		}else{
+			$('.cardones').removeClass('movelefts');
+			$('.cardtwos').removeClass('movebottoms');
+			$('.cardthrees').removeClass('moverights');
+		}
+
+	});
+
+	// End Pricing Section
+
+	// Start Join Us Section
+
+		$('#accordion').accordion();
+
+	// End Join Us Section
+
+	// Start Footer Section
+
+	$('#getyear').text(new Date().getUTCFullYear());
+
+	// End Footer Section
+
+	// Start Progress
+
+	
+
+	$(window).scroll(function(){
+
+		var getprogress = $('#progresses');
+		var getprogressval = $('#progressvalues');
+		var getscrolltop = $(this).scrollTop();
+		// console.log(getscrolltop);
+
+		// By jQuery
+
+		// var getscrollheight = $(document).height();
+		// // console.log(getscrollheight);
+		// var getclientheight = $(window).height();
+		// // console.log(getclientheight);
+		// var calcheight= getscrollheight - getclientheight;
+		// var getfinalheight = Math.round(getscrolltop * 100 / calcheight);
+		// // console.log(finalheight);
+
+		// By javaScript
+
+		var getscrollheight = document.documentElement.scrollHeight;
+		// console.log(getscrollheight);
+		var getclientheight = document.documentElement.clientHeight;
+		// console.log(getclientheight);
+		var calcheight= getscrollheight - getclientheight;
+		var getfinalheight = Math.round(getscrolltop * 100 / calcheight);
+		// // console.log(finalheight);
+
+		getprogressval.text(`${getfinalheight}%`);
+		getprogress.css({
+			'background':`conic-gradient(steelblue ${getfinalheight}%,#eee ${getfinalheight}%)`
+		});
+	});
+
+	// End Progress 
+
 });
